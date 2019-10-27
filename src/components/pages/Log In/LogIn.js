918d4cf7bd310d41a9cb1d342ef1a918e833387a
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2019-10-27 22:25:19 +0700

    change message in index file

--- src/components/pages/Log In/LogIn.js
+++ src/components/pages/Log In/LogIn.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './LogIn.css';
 function LogIn(props){
+    //declare state
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+   
     //define submit handling function
-    function submitHandling(){
+    async function submitHandling(event){
+        const data={email,password};
+       
+        setEmail('');
+        setPassword('');
+        event.preventDefault();
 
     }
+    //define onchange email and password function
+    function onChangeEmail(event){
+        setEmail(event.target.value);
+    }
+
+    function onChangePassword(event){
+        setPassword(event.target.value);
+    }
     return (
-        <Form>
+        <Form onSubmit={(data)=>submitHandling(data)}>
             <FormGroup>
                 <Label for="exampleEmail">Email:</Label>
-                <Input type="email" name="email" id="exampleEmail" placeholder="example@email.com" />
+                <Input type="email" 
+                       name="email" 
+                       id="exampleEmail" 
+                       placeholder="example@email.com" 
+                       value={email} 
+                       onChange={(event)=>onChangeEmail(event)} />
             </FormGroup>
 
             <FormGroup>
                 <Label for="examplePassword">Password:</Label>
-                <Input type="password" name="password" id="examplePassword" placeholder="password" />
+                <Input type="password"
+                       name="password" 
+                       id="examplePassword"
+                       placeholder="password" 
+                       value={password} 
+                       onChange={(event)=>onChangePassword(event)}/>
             </FormGroup>
             <Button>Submit</Button>
       </Form>

f345645c64cebc958727adb03675339c686e0ed7
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2019-10-27 21:27:18 +0700

    Redirect to Login page from Home page when haven't login, developing the Login page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
-
+import Cookies from 'js-cookie';
+import Login from '../Log In/LogIn';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+  } from "react-router-dom";
+  
 function Home(props){
+    const authCookie= Cookies.get('authID');
+    console.log(authCookie);
     return (
-       <div>
-        <h1>Home Page</h1>
-       </div>
+        <Router>
+            <div>
+                {authCookie===undefined && <Redirect to='/login'/>}
+                {authCookie!==undefined && <h1>HOME PAGE</h1>}
+            </div>
+            <Switch>
+                <Route path='/login' exact>
+                    <Login/>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
--- src/components/pages/Log In/LogIn.js
+++ src/components/pages/Log In/LogIn.js
@@ -1,8 +1,25 @@
 import React from 'react';
-
+import axios from 'axios';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import './LogIn.css';
 function LogIn(props){
+    //define submit handling function
+    function submitHandling(){
+
+    }
     return (
-        <h1>Log In Page</h1>
+        <Form>
+            <FormGroup>
+                <Label for="exampleEmail">Email:</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="example@email.com" />
+            </FormGroup>
+
+            <FormGroup>
+                <Label for="examplePassword">Password:</Label>
+                <Input type="password" name="password" id="examplePassword" placeholder="password" />
+            </FormGroup>
+            <Button>Submit</Button>
+      </Form>
     );
 }
 

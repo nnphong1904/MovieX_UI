46226b2fbcb941be0ca3091754a8fb966b01bf06
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2019-10-28 10:32:11 +0700

    recived token from API server when login success

--- src/components/pages/Log In/LogIn.js
+++ src/components/pages/Log In/LogIn.js
@@ -1,16 +1,27 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import Movie from '../Movies/Movies';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+  } from "react-router-dom";
 import './LogIn.css';
 function LogIn(props){
     //declare state
     const [email,setEmail]=useState('');
     const [password,setPassword]=useState('');
+    const [authToken, setAuthToken] =useState('');
    
     //define submit handling function
     async function submitHandling(event){
         const data={email,password};
-       
+        axios.post('http://localhost:4000/api/login',data)
+            .then(res=>{setAuthToken(res.data)
+                    console.log(res.data)});
         setEmail('');
         setPassword('');
         event.preventDefault();
@@ -25,28 +36,37 @@ function LogIn(props){
         setPassword(event.target.value);
     }
     return (
-        <Form onSubmit={(data)=>submitHandling(data)}>
-            <FormGroup>
-                <Label for="exampleEmail">Email:</Label>
-                <Input type="email" 
-                       name="email" 
-                       id="exampleEmail" 
-                       placeholder="example@email.com" 
-                       value={email} 
-                       onChange={(event)=>onChangeEmail(event)} />
-            </FormGroup>
+        <Router>
+            {authToken!=='' && <Redirect to='/movies'/>}
+            {authToken==='' && <Form onSubmit={(data)=>submitHandling(data)}>
+                    <FormGroup>
+                        <Label for="exampleEmail">Email:</Label>
+                        <Input type="email" 
+                            name="email" 
+                            id="exampleEmail" 
+                            placeholder="example@email.com" 
+                            value={email} 
+                            onChange={(event)=>onChangeEmail(event)} />
+                    </FormGroup>
 
-            <FormGroup>
-                <Label for="examplePassword">Password:</Label>
-                <Input type="password"
-                       name="password" 
-                       id="examplePassword"
-                       placeholder="password" 
-                       value={password} 
-                       onChange={(event)=>onChangePassword(event)}/>
-            </FormGroup>
-            <Button>Submit</Button>
-      </Form>
+                    <FormGroup>
+                        <Label for="examplePassword">Password:</Label>
+                        <Input type="password"
+                            name="password" 
+                            id="examplePassword"
+                            placeholder="password" 
+                            value={password} 
+                            onChange={(event)=>onChangePassword(event)}/>
+                    </FormGroup>
+                    <Button>Submit</Button>
+            </Form>}
+            <Switch>
+                <Route path='/movies'>
+                    <Movie/>
+                </Route>
+            </Switch>
+        </Router>
+      
     );
 }
 
